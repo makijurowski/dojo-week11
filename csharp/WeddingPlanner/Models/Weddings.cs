a6f2515fdebbd5eb39f2b7ac5a11cb04ed8a6942
makiroggers <makiroggers@gmail.com>
2017-10-30 13:24:10 -0700

    Added pomelo entity framework for MySql

---
+++ csharp/WeddingPlanner/Models/Weddings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeddingPlanner
+{
+    public partial class Weddings
+    {
+        public Weddings()
+        {
+            Rsvps = new HashSet<Rsvps>();
+        }
+
+        public int WeddingId { get; set; }
+        public string BrideName { get; set; }
+        public string GroomName { get; set; }
+        public DateTime? Date { get; set; }
+        public string Address { get; set; }
+        public int UserId { get; set; }
+
+        public Users User { get; set; }
+        public ICollection<Rsvps> Rsvps { get; set; }
+    }
+}
