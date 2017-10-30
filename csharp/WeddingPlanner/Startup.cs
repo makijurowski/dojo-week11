7e86fd16da0d4990249e9d3a23607e035f5218ef
makiroggers <makiroggers@gmail.com>
2017-10-30 16:06:39 -0700

    Added materialize framework

--- csharp/WeddingPlanner/Startup.cs
+++ csharp/WeddingPlanner/Startup.cs
@@ -23,7 +23,6 @@ namespace WeddingPlanner
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -33,13 +32,11 @@ namespace WeddingPlanner
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -54,9 +51,7 @@ namespace WeddingPlanner
             }
 
             app.UseStaticFiles();
-
             app.UseAuthentication();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
