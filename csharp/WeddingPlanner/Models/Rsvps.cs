607f7c7e7ebfe1a84cb1e79c0886182efa004987
makiroggers <makiroggers@gmail.com>
2017-10-30 15:34:49 -0700

    Added entity-generated models.

---
+++ csharp/WeddingPlanner/Models/Rsvps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeddingPlanner
+{
+    public partial class Rsvps
+    {
+        public int RsvpId { get; set; }
+        public int UsersUserId { get; set; }
+        public int WeddingId { get; set; }
+        public int UserId { get; set; }
+
+        public Users UsersUser { get; set; }
+        public Weddings Weddings { get; set; }
+    }
+}
---
+++ csharp/WeddingPlanner/Models/wedding_plannerContext.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using WeddingPlanner.Models;
+
+namespace WeddingPlanner
+{
+    public partial class wedding_plannerContext : DbContext
+    {
+        public virtual DbSet<Rsvps> Rsvps { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Weddings> Weddings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=127.0.0.1;User Id=root;Password=root;Database=wedding_planner");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Rsvps>(entity =>
+            {
+                entity.HasKey(e => new { e.RsvpId, e.UsersUserId, e.WeddingId, e.UserId });
+
+                entity.ToTable("rsvps");
+
+                entity.HasIndex(e => e.UsersUserId)
+                    .HasName("fk_rsvps_users1_idx");
+
+                entity.HasIndex(e => new { e.WeddingId, e.UserId })
+                    .HasName("fk_rsvps_weddings1_idx");
+
+                entity.Property(e => e.RsvpId)
+                    .HasColumnName("rsvp_id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.UsersUserId)
+                    .HasColumnName("users_user_id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.WeddingId)
+                    .HasColumnName("wedding_id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("user_id")
+                    .HasColumnType("int(11)");
+
+                entity.HasOne(d => d.UsersUser)
+                    .WithMany(p => p.Rsvps)
+                    .HasForeignKey(d => d.UsersUserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_rsvps_users1");
+
+                entity.HasOne(d => d.Weddings)
+                    .WithMany(p => p.Rsvps)
+                    .HasForeignKey(d => new { d.WeddingId, d.UserId })
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_rsvps_weddings1");
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.ToTable("users");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("user_id")
+                    .HasColumnType("int(11)")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.FirstName)
+                    .HasColumnName("first_name")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.LastName)
+                    .HasColumnName("last_name")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Password)
+                    .HasColumnName("password")
+                    .HasMaxLength(255);
+            });
+
+            modelBuilder.Entity<Weddings>(entity =>
+            {
+                entity.HasKey(e => new { e.WeddingId, e.UserId });
+
+                entity.ToTable("weddings");
+
+                entity.HasIndex(e => e.UserId)
+                    .HasName("fk_weddings_users_idx");
+
+                entity.Property(e => e.WeddingId)
+                    .HasColumnName("wedding_id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("user_id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Address)
+                    .HasColumnName("address")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.BrideName)
+                    .HasColumnName("bride_name")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Date)
+                    .HasColumnName("date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.GroomName)
+                    .HasColumnName("groom_name")
+                    .HasMaxLength(45);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Weddings)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_weddings_users");
+            });
+        }
+    }
+}
