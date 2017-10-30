1bd55704989e3f8f1215da2ca7413de7b2a7a47a
makiroggers <makiroggers@gmail.com>
2017-10-29 20:00:38 -0700

    Added password hashing

--- csharp/DojoLeague/Controllers/HomeController.cs
+++ csharp/DojoLeague/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using DojoLeague.Factories;
 using DojoLeague.Models;
 
@@ -25,18 +26,33 @@ namespace DojoLeague.Controllers
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register(User user)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            if(ModelState.IsValid)
+            {
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                user.Password = Hasher.HashPassword(user, user.Password);
+            }
+            return View("Index");
         }
 
-        public IActionResult Contact()
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(string Email, string CheckPassword)
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            var user = userFactory.FindByEmail(Email);
+            if (user != null && CheckPassword != null)
+            {
+                var Hasher = new PasswordHasher<User>();
+                if (Hasher.VerifyHashedPassword(user, user.Password, CheckPassword) != 0)
+                {
+                    // Success
+                }
+            }
+            // Failure
+            return View("Index");
         }
 
         public IActionResult Error()
