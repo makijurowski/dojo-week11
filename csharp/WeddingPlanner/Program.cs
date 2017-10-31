d6e82d4198ec14287706dd7faffb566675cf3c4b
makiroggers <makiroggers@gmail.com>
2017-10-30 18:43:17 -0700

    Aligning

--- csharp/WeddingPlanner/Program.cs
+++ csharp/WeddingPlanner/Program.cs
@@ -20,6 +20,7 @@ namespace WeddingPlanner
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
